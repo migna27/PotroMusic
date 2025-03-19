8faf2d1ed8a3a295c3e3d33c7e4ae5078a18ff4d
Miguel <Miguel@192.168.1.21>
2025-03-18 22:11:34 -0700

    se comento

--- src/mx/itson/music/entidades/Cancion.java
+++ src/mx/itson/music/entidades/Cancion.java
@@ -9,6 +9,48 @@ package mx.itson.music.entidades;
  * @author alumnog
  */
 public class Cancion {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the duracion
+     */
+    public int getDuracion() {
+        return duracion;
+    }
+
+    /**
+     * @param duracion the duracion to set
+     */
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    /**
+     * @return the orden
+     */
+    public int getOrden() {
+        return orden;
+    }
+
+    /**
+     * @param orden the orden to set
+     */
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
     private String nombre;
     private int duracion;
     private int orden;
--- src/mx/itson/music/ui/Main.java
+++ src/mx/itson/music/ui/Main.java
@@ -6,6 +6,7 @@ package mx.itson.music.ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import mx.itson.music.entidades.Album;
 import mx.itson.music.entidades.Artista;
 import mx.itson.music.entidades.Cancion;
@@ -17,18 +18,68 @@ import mx.itson.music.enums.Genero;
  */
 public class Main {
     public static void main(String[] args) {
-        Album a = new Album();
+        Scanner scan =new Scanner(System.in);
+        Album album = new Album();
+        
+        /*
         Artista black = new Artista();
         black.setNombre("black pink");
-        
-        
         a.setNombre("Born Pink");
-       a.setLanzamiento(2022);
-       a.setGenero(Genero.POP);
-     
+        a.setLanzamiento(2022);
+        a.setGenero(Genero.POP);
+        */
+        System.out.println("Ingrese el nombre del album");
+       String nombreAlbum = scan.nextLine();
+       album.setNombre(nombreAlbum);
+      try{ 
+       Artista artis = new Artista();
+       System.out.println("Ingrese el nombre del artista");
+       String artista = scan.nextLine();
+       artis.setNombre(artista);
+       album.setArtista(artis);
        
+       //definimos valores de atributo por defecto para artista
+       artis.setDescripcion("Cantante de musica");
+       artis.setSitio("www.artis.com");
+       album.setGenero(Genero.OTRO);
        
        List<Cancion> canciones = new ArrayList<>();
-        a.setCanciones(canciones);
+        album.setCanciones(canciones);
+        Cancion cancion = new Cancion();
+            OUTER:
+            while (true) {
+                System.out.println("agregar una nueva cancion 1(si) o 2(no)");
+                String opcion = scan.nextLine();
+                switch (opcion) {
+                    case "1":
+                        System.out.println("Ingrese el nombre de la canción: ");
+                        String nombreCancion = scan.nextLine();
+                        cancion.setNombre(nombreCancion);
+                        System.out.println("Ingrese el orden de la canción: ");
+                        int ordenCancion = scan.nextInt();
+                        scan.nextLine(); 
+                        cancion.setOrden(ordenCancion);
+                        System.out.println("ingrese la duracion de la cancion");
+                        int duracion = scan.nextInt();
+                        cancion.setDuracion(duracion);
+                        canciones.add(cancion); 
+                        System.out.println("¡Canción registrada exitosamente!");
+                        break;
+                    case "2":
+                        System.out.println("Registro de canciones finalizado.");
+                        break OUTER;
+                    default:
+                        System.out.println("elija una opcion valida");
+                        break;
+                }
+            }
+        for(Cancion c : album.getCanciones()){
+           System.out.println(c.getOrden());
+           System.out.println(c.getNombre());
+           System.out.println(c.getDuracion());
+        }
+    }catch(Exception e) {
+    System.err.println("ocurrio un error en la ejecucion");
+    }
     }
 }
